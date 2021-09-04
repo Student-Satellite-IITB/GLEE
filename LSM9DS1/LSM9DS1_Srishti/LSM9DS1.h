4abda2a250a3551068cd6b592b27932cea409a8a
Vatsal147 <54111398+Vatsal147@users.noreply.github.com>
2021-09-04 18:17:31 +0530

    Added magnetometer register mapping code

--- LSM9DS1/LSM9DS1_Srishti/LSM9DS1.h
+++ LSM9DS1/LSM9DS1_Srishti/LSM9DS1.h
@@ -5,7 +5,7 @@
 #define SADW  0xD4
 #define SADR  0xD5
 
-//Registers Address
+// Acc. and Gyro. Registers Address
 #define ACT_THS 			0x04
 #define ACT_DUR 			0x05
 #define INT_GEN_CFG_XL 		0x06
@@ -56,6 +56,31 @@
 #define INT_GEN_THS_ZH_G 	0x35
 #define INT_GEN_THS_ZL_G 	0x36
 #define INT_GEN_DUR_G 		0x37
+
+// Magneto. Register Address
+#define OFFSET_X_REG_L_M	0x05
+#define OFFSET_X_REG_H_M	0x06
+#define OFFSET_Y_REG_L_M	0x07
+#define OFFSET_Y_REG_H_M	0x08
+#define OFFSET_Z_REG_L_M	0x09
+#define OFFSET_Z_REG_H_M	0x0A
+#define WHO_AM_I_M			0x0F
+#define CTRL_REG1_M			0x20
+#define CTRL_REG2_M			0x21
+#define CTRL_REG3_M			0x22
+#define CTRL_REG4_M			0x23
+#define CTRL_REG5_M			0x24
+#define STATUS_REG_M		0x27
+#define OUT_X_L_M			0x28
+#define OUT_X_H_M			0x29
+#define OUT_Y_L_M			0x2A
+#define OUT_Y_H_M			0x2B
+#define OUT_Z_L_M			0x2C
+#define OUT_Z_H_M			0x2D
+#define INT_CFG_M			0x30
+#define INT_SRC_M			0x31
+#define INT_THS_L_M			0x32
+#define INT_THS_H_M			0x33
 
 //Variables to store sensor data
 
