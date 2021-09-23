1f708e3535db11d482fb85395cddf4909e1ff67c
Akshat Vira <55907237+ViraAkshat@users.noreply.github.com>
2021-09-23 13:07:08 +0530

    Updated Measurement Function

--- Si7021/Si7021_Akshat/Si7021.c
+++ Si7021/Si7021_Akshat/Si7021.c
@@ -11,17 +11,27 @@
 
 // Sensor functions
 uint16_t measurement(uint8_t cmd) {
+	//Uncomment this when checksum is implemented
+	//uint8_t n_bytes = 3; // MSB + LSB + Checksum
+	//if (cmd == 0xE0) n_bytes = 2; //No checksum transfered when previous temperature (from RH measurement) is read
+	
+	uint8_t n_bytes = 2; // MSB + LSB
+	
 	TWI_init();
 	TWI_start();
 	TWI_SLA_W(SI7021_ADDRESS + 0x01);  // Address + Write bit
 	TWI_transmit(cmd);
 	TWI_Repeated_Start();
 	TWI_SLA_W(SI7021_ADDRESS);
+	
 	uint8_t msb = TWI_receive(0);   // MSB
 	uint8_t lsb = TWI_receive(1);   // LSB
+	
+	uint8_t checksum = TWI_receive(1) // 
+	
 	TWI_stop();
 	
-	lsb &= 0xFC; // LSB is of the form 0bxxxxxx00
+	// lsb &= 0xFC; // LSB is of the form 0bxxxxxx00
 	uint16_t measured_value = (msb << 8) | lsb;
 }
 
