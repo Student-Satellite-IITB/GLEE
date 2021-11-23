5c40b544a91ad3a9ebe693cc912925bc62a429ce
Srishti Sharma <81479041+srishtis19@users.noreply.github.com>
2021-11-23 17:05:46 +0530

    Add ISR, function to configure external interrupts

--- LSM9DS1/LSM9DS1_Srishti/main.c
+++ LSM9DS1/LSM9DS1_Srishti/main.c
@@ -8,11 +8,32 @@
 #include "I2C.c"
 #include "LSM9DS1.c"
 
+void configure_ext_interrupt(){
+	EIMSK|=(1<<INT0);						//Enable INT0
+	EICRA|=(1<<ISC00)|(1<<ISC01);					//Configure INT0 as rising edge triggered
+		
+	sei();								//Global interrupt enable
+}
+
+ISR(INT0_vect){
+	
+	for(int i=0;i<32;i++){
+		read_acc(); 						//Acc data gets stored in Ax, Ay, Az
+		_delay_ms(100);						//Rate of reading data from FIFO to be decided; should be > ODR for no overwrite
+	}	
+
+}
+
+
 int main(void)
 {
 	uint8_t data_available= acc_data_available();
 	
 	acc_init();							//Initialize Accelerometer
+	//Cont_init();
+	
+	//configure_ext_interrupt();
+	//configure_interrupt();					//Enable interrupt generation when FIFO is full (32 unread samples)
 	
 	while(1){
 		
