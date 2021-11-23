042d9cb7e5398119f07265ad28c1ddc9bb572ead
Akshat Vira <55907237+ViraAkshat@users.noreply.github.com>
2021-11-23 17:55:39 +0530

    Update Si7021.c

--- Si7021/Si7021_Akshat/Si7021.c
+++ Si7021/Si7021_Akshat/Si7021.c
@@ -9,6 +9,25 @@
 #include "Si7021.h"
 #include "I2C.h"
 
+// Electronic ID
+//uint16_t readElecID () {
+	//TWI_start();
+	//TWI_SLA_W(SI7021_ADDRESS + 0x01);
+	//TWI_transmit(SI7021_READ_ID1);
+//}
+
+uint8_t readFirmRev() {
+	TWI_start();
+	TWI_SLA_W(SI7021_ADDRESS + 0x01);
+	TWI_transmit(SI7021_READ_FIRM_REV_1);
+	TWI_transmit(SI7021_READ_FIRM_REV_2);
+	TWI_Repeated_Start();
+	TWI_SLA_W(SI7021_ADDRESS);
+	uint8_t firm = TWI_receive(1);
+	TWI_stop();
+	return firm;
+}
+
 // Sensor functions
 uint16_t measurement(uint8_t cmd) {
 	//Uncomment this when checksum is implemented
@@ -27,34 +46,41 @@ uint16_t measurement(uint8_t cmd) {
 	uint8_t msb = TWI_receive(0);   // MSB
 	uint8_t lsb = TWI_receive(1);   // LSB
 	
-	uint8_t checksum = TWI_receive(1) // 
+	uint8_t checksum = TWI_receive(1); // 
 	
 	TWI_stop();
 	
 	// lsb &= 0xFC; // LSB is of the form 0bxxxxxx00
 	uint16_t measured_value = (msb << 8) | lsb;
+	
+	return measured_value;
 }
 
-uint16_t getRH(bool hold) {
+uint16_t getRH(uint8_t hold) {
 	//master no hold mode
 	if(!hold) {
 		uint16_t rh = measurement(SI7021_MSR_RH_NO_HOLD);
 		return rh;
 	}
-	//master hold mode
-	else {
-		
-	}
 }
 
-uint8_t getTemperature(bool hold) {
+uint8_t getTemperature(uint8_t hold) {
 	//master no hold mode
 	if(!hold) {
 		uint16_t rh = measurement(SI7021_MSR_TEMP_NO_HOLD);
 		return rh;
 	}
-	//master hold mode
-	else {
-		
-	}
+
+}
+
+uint8_t getPrevTemp(uint8_t hold) {
+		//master no hold mode
+		if(!hold) {
+			uint16_t temp = measurement(SI7021_READ_TEMP_PREV_RH);
+			return temp;
+		}
+		//master hold mode
+		else {
+			
+		}
 }
