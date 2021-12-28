ec659d725812b957fb3d17884972c1de6e7f1768
Srishti Sharma <81479041+srishtis19@users.noreply.github.com>
2021-12-28 17:20:17 +0530

    define UART related functions

---
+++ LSM9DS1/LSM9DS1_Srishti/UART.c
@@ -0,0 +1,19 @@
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+void UART_init(long USART_BAUDRATE)
+{
+	UCSR1B |= (1 << RXEN1) | (1 << TXEN1);	/* Turn on transmission and reception */
+	UCSR1C |= (1 << USBS1) | (1 << UCSZ10) | (1 << UCSZ11);/* Use 8-bit char size */
+	UBRR1L = BAUD_PRESCALE;			/* Load lower 8-bits of the baud rate */
+	UBRR1H = (BAUD_PRESCALE >> 8);		/* Load upper 8-bits*/
+}
+unsigned char UART_RxChar()
+{
+	while ((UCSR1A & (1 << RXC1)) == 0);/* Wait till data is received */
+	return(UDR1);		/* Return the byte */
+}
+void UART_TxChar(char ch)
+{
+	while (! (UCSR1A & (1<<UDRE1)));  /* Wait for empty transmit buffer */
+	UDR1 = ch ;
+}
