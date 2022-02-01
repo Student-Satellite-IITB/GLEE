116fee33c3ed4bdbf67ff9df768546d48f44922d
advaitmehla <77231014+advaitmehla@users.noreply.github.com>
2022-02-01 23:36:26 +0530

    Renamed folder

--- GLEE_sim/build/CMakeFiles/G4_Tutorial.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/G4_Tutorial"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/G4_Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- GLEE_sim/build/CMakeFiles/GLEE_SIM.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GLEE_SIM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GLEE_SIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
