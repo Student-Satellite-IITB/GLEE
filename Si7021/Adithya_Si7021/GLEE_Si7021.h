cbb67b47dc90b6e020f1d725fac392d19b45990b
Adithya Raj Mishra <81479670+armishr@users.noreply.github.com>
2022-05-02 17:28:39 +0530

    Update GLEE_Si7021.h
    Header updated

--- Si7021/Adithya_Si7021/GLEE_Si7021.h
+++ Si7021/Adithya_Si7021/GLEE_Si7021.h
@@ -1,221 +1,48 @@
 /*
- * Si7021.c
+ * Si7021.h
  *
- * Created: 03-09-2021 21:21:39
+ * Created: 03-09-2021 21:19:44
  *  Author: Akshat
  */ 
-#define F_CPU 8000000 // 16MHz Clock frequency
-#include <util/delay.h>
-#include <avr/io.h>
-#include <stdbool.h>
-#include "Si7021.h"
-#include "I2C.h"
 
 
-// check whether it is si7021 using EID
-uint8_t SI7021_getDevice() {
-	TWI_start();
-	TWI_SLA_W(SI7021_ADDRESS<<1);
-	TWI_transmit(SI7021_READ_EID_SNB_1);
-	TWI_transmit(SI7021_READ_EID_SNB_2);
-	TWI_Repeated_Start();   
-	TWI_transmit((SI7021_ADDRESS<<1) + 0x01);
-	uint8_t snb_1=TWI_receive(1);
-	TWI_stop();
-	return(snb_1);
-	//TWI_transmit(SI7021_READ_ID1);
-}
+#ifndef SI7021_H_
+#define SI7021_H_
 
-uint8_t SI7021_readFirmRev() {
-	TWI_start();
-	TWI_SLA_W((SI7021_ADDRESS<<1) + 0x00);
-	TWI_transmit(SI7021_READ_FIRM_REV_1);
-	TWI_transmit(SI7021_READ_FIRM_REV_2);
-	TWI_Repeated_Start();
-	TWI_SLA_W((SI7021_ADDRESS<<1) + 0x01);
-	uint8_t firm = TWI_receive(0);
-	TWI_stop();
-	return firm;
-}
-float SI7021_Get_Measurment(uint8_t hold_command,uint8_t no_hold_command,int hold,int check){
-	uint8_t msb,lsb;
-	TWI_start();
-	TWI_transmit((SI7021_ADDRESS<<1)+0x00);
-	if(hold){
-		TWI_transmit(hold_command);
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		_delay_us(100);
-	}
-	else{
-		
-		TWI_transmit(no_hold_command);
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		while((TWSR&0xF8)==0x48){
-			TWI_start();
-			TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-			_delay_us(10);
-		}
-	}
-	msb=TWI_receive(0);
-	if(check){
-		lsb=TWI_receive(0);
-		uint8_t checksum=TWI_receive(1);
-		//idk do checksum algo
-		TWI_stop();
-		float val=((msb<<8)|lsb);
-		if(!SI7021_error_check(val)){
-			val*=175.72;
-			val/=65536;
-			val-=46.85;
-			return val;
-		}
-		else{
-			return -1;
-		}
+// Sensor address
+#define SI7021_ADDRESS           0x40                         // Slave (Si7021) Address
 
-	}
-	else{
-		lsb=TWI_receive(1);
-		TWI_stop();
-		float val=((msb<<8)|lsb);
-		val*=175.72;
-		val/=65536;
-		val-=46.85;
-		return val;
-	}
-	
-}
-/*int SI7021_error_check(uint16_t data, uint8_t checkSum){
-	uint32_t poly=0x131;//polynomial x8+x5+x4+1
-	uint32_t num=data<<8;
-	for
-	return data;
-}*/
-
-// Sensor functions
-/*uint16_t measurement(uint8_t cmd) {
-	//Uncomment this when checksum is implemented
-	//uint8_t n_bytes = 3; // MSB + LSB + Checksum
-	//if (cmd == 0xE0) n_bytes = 2; //No checksum transfered when previous temperature (from RH measurement) is read
-	
-
-	
-	TWI_init();
-	TWI_start();
-	TWI_SLA_W(SI7021_ADDRESS + 0x01);  // Address + Write bit
-	TWI_transmit(cmd);
-	TWI_Repeated_Start();
-	TWI_SLA_W(SI7021_ADDRESS);
-	
-	uint8_t msb = TWI_receive(0);   // MSB
-	uint8_t lsb = TWI_receive(1);   // LSB
-	
-	//uint8_t checksum = TWI_receive(1); // 
-	
-	TWI_stop();
-	
-	// lsb &= 0xFC; // LSB is of the form 0bxxxxxx00
-	uint16_t measured_value = (msb << 8) | lsb;
-	
-	return measured_value;
-}*/
-
-/*float getRH(uint8_t hold) {
-	
-	if(hold){
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x00);
-		TWI_transmit(SI7021_MSR_RH_HOLD);
-		TWI_Repeated_Start();	
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		//_delay_ms(1);
-		
-		uint8_t msb=TWI_receive(0);
-		uint8_t lsb=TWI_receive(1);
-		uint8_t rh=(lsb);
-		
-		
-		TWI_stop();
-		
-		return rh; 
-		
-	}
-	else{
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x00);
-		TWI_transmit(SI7021_MSR_RH_NO_HOLD);
-		while(TWDR!=0){
-			TWI_transmit(SI7021_ADDRESS+0x00);
-		}
-		uint8_t lsb=TWI_receive(true);
-		uint8_t msb=TWI_receive(true);
-		return(lsb);
-	}
-}*/
-float SI7021_Get_RH(int hold,int check){
-	return (SI7021_Get_Measurment(SI7021_MSR_RH_HOLD,SI7021_MSR_RH_NO_HOLD,hold,check));
-}
-/*float getTemperature(uint8_t hold,int check) {
-	uint8_t msb,lsb;
-	TWI_start();
-	TWI_transmit((SI7021_ADDRESS<<1)+0x00);
-	if(hold){
-		TWI_transmit(SI7021_MSR_TEMP_HOLD);
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		_delay_us(100);
-	}
-	else{
-		
-		TWI_transmit(SI7021_MSR_TEMP_NO_HOLD);
-		TWI_start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		while((TWSR&0xF8)==0x48){
-			TWI_start();
-			TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-			_delay_us(10);			
-		}
-	}
-	msb=TWI_receive(0);
-	if(check){
-		lsb=TWI_receive(0);
-		uint8_t checksum=TWI_receive(1);
-		//idk do checksum algo
-		int isOk=error_check(temp,checksum);
-	}
-	else{
-		lsb=TWI_receive(1);
-	}
-	TWI_stop();
-	float temp=((msb<<8)|lsb);
-	temp*=175.72;
-	temp/=65536;
-	temp-=46.85;
-	return temp;
-
-}*/
-float SI7021_Get_Temperature(int hold,int check) {
-	return (SI7021_Get_Measurment(SI7021_MSR_TEMP_HOLD,SI7021_MSR_TEMP_NO_HOLD,hold,check));
-}
+//Command description
+#define SI7021_MSR_RH_HOLD       0xE5                         // Measure RH, Hold master mode
+#define SI7021_MSR_RH_NO_HOLD    0xF5                         // Measure RH, No hold master mode
+#define SI7021_MSR_TEMP_HOLD     0xE3                         // Measure Temperature, Hold master mode
+#define SI7021_MSR_TEMP_NO_HOLD  0xF3                         // Measure Temperature, No hold master mode
+#define SI7021_READ_TEMP_PREV_RH 0xE0                         // Read temperature from previous RH measurement
+#define SI7021_RESET             0xFE                         // Reset the sensor
+#define SI7021_WRITE_RHT_UR1     0xE6                         // Write to RH/T to user register 1
+#define SI7021_READ_RHT_UR1      0xE7                         // Read RH/T from user register 1
+#define SI7021_WRITE_HEATER_CR   0x51                         // Write Heater Control Register
+#define SI7021_READ_HEATER_CR    0x11                         // Read Heater Control Register
+#define SI7021_READ_ID1          0xFA                         // Read 1st byte of electronic ID
+#define SI7021_READ_ID2          0xFC                         // Read 2nd byte of electronic ID
+#define SI7021_READ_FIRM_REV_1     0x84                         // Read firmware revision version
+#define SI7021_READ_FIRM_REV_2     0xB8                         // Read firmware revision version
+#define SI7021_READ_EID_SNB_1		0xFC
+#define SI7021_READ_EID_SNB_2		0xC9
+#define SI7021_READ_EID_SNA_1		0XFA
+#define SI7021 READ_EID_SNA_2		0x0F
+//Sensor functions
+uint8_t readElecID();                                         // To read electronic ID (pending)
+uint8_t readFirmRev();
+uint16_t measurement(uint8_t cmd);                             // To make measurement (pending)
+float getRH(uint8_t hold);                                     // To read Relative Humidity value (pending)
+float getTemperature(uint8_t hold,int check);                            // To read Temperature value (pending)
+float SI7021_getTemperature(int hold,int check);                            // To read Temperature value (pending)
+float SI7021_getRH(int hold,int check);                                     // To read Relative Humidity value (pending)
+uint8_t getPrevTemp(int hold);                                        // To read temperature that was measured along with RH
+void reset();                                                 // To reset the sensor (pending)
+void enableHeater();                                          // To enable or disable heater (pending)
+void heaterLevel();                                           // To set heater level (pending)
 
 
-
-uint8_t getPrevTempRh(uint8_t hold) {
-		TWI_start();
-		TWI_transmit(SI7021_ADDRESS<<1);
-		TWI_transmit(SI7021_READ_TEMP_PREV_RH);
-		TWI_Repeated_Start();
-		TWI_transmit((SI7021_ADDRESS<<1)+0x01);
-		uint8_t msb=TWI_receive(0);
-		uint8_t lsb=TWI_receive(1);
-		TWI_stop();
-		uint8_t temp=(msb<<8)|(lsb);
-		temp*=175.72;
-		temp/=65536;
-		temp-=46.85;
-		return temp;
-		
-		
-}
+#endif /* SI7021_H_ */
